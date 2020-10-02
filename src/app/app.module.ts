2a40dc278f8815f59487def5b0a60a762bf206fc
Matheus Mastrocinque <tec.mastrocinque@gmail.com>
2020-10-02 11:46:16 -0300

    Adicionando componente nav-bar e error-404 ao core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    Error404Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
-      
-      {
-        path: '**', component: Error404Component
-      }
     ])
   ],
   providers: [],
